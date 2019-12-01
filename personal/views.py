2d9896e1f8a0181ddbd8814b662b87c43dc7f79e
asfrank <asfrank@qq.com>
2019-12-01 21:18:14 +0800

    Initial commit

---
+++ personal/views.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.utils.decorators import method_decorator
+from django.views.generic.base import View
+from django.contrib import auth
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, "index.html")
+
+class LoginView(View):
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+        if username == "" or password == "":
+            return render(request, "index.html", {
+                "error": "用户名或密码为空"
+            })
+        user = auth.authenticate(username=username, password= password)
+        if user is None:
+            return render(request, "index.html", {
+                "error": "用户名或密码错误"
+            })
+
+        auth.login(request, user)
+        return HttpResponseRedirect("/project")
+
+class LogoutView(View):
+    @method_decorator(login_required)
+    def get(self, request):
+        auth.logout(request)
+        return HttpResponseRedirect("/")
+
+
+class ProjectView(View):
+    @method_decorator(login_required)
+    def get(self, request):
+        return render(request, "project.html")
+
+class ModuleView(View):
+    @method_decorator(login_required)
+    def get(self, request):
+        return render(request, "module.html")
+
+
+
