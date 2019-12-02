c2c1352e0250021eb9d6abbdfcce8da8c90a6783
asfrank <asfrank@qq.com>
2019-12-02 23:44:00 +0800

    project model

--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    describe = models.TextField(default="")
+    status = models.BooleanField(default=1)
+    create_time = models.DateTimeField(auto_now_add=True)
+
