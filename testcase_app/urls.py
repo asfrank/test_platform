de35ea7c1d4f877d3b699896846c0757f602e161
asfrank <asfrank@qq.com>
2019-12-22 01:15:43 +0800

    接口调试及断言

--- testcase_app/urls.py
+++ testcase_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from testcase_app.views import TestcaseView, CaseDebugView
+from testcase_app.views import TestcaseView, CaseDebugView, CaseAssertView
 
 urlpatterns = [
     path('', TestcaseView.as_view(), name="testcase"),
     path('debug/', CaseDebugView.as_view(), name='casedebug'),
+    path('assert/', CaseAssertView.as_view(), name='caseassert')
 ]
--- testcase_app/views.py
+++ testcase_app/views.py
@@ -18,14 +18,59 @@ class CaseDebugView(View):
     def post(self, request):
         url = request.POST.get("url", "")
         method = request.POST.get("method", "")
-        header = request.POST.get("header", "")
+        header = request.POST.get("header", "") or "{}"
         param_type = request.POST.get("param_type", "")
-        parameter = request.POST.get("parameter", "")
-        if parameter:
-            parameter = json.loads(parameter)
+        parameter = request.POST.get("parameter", "") or "{}"
+
+        json_header = header.replace("\'", "\"")
+        try:
+            header = json.loads(json_header)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({"result": "header类型错误"})
+
+        json_param = parameter.replace("\'", "\"")
+        try:
+            payload = json.loads(json_param)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({"result": "参数类型错误"})
 
         if method == "get":
-            r = requests.get(url=url)
-            return JsonResponse({"result": r.text})
-        return JsonResponse({"result": "ok"})
+            if header == "":
+                r = requests.get(url=url, params=payload)
+            else:
+                r = requests.get(url=url, params=payload, headers=header)
+
+        if method == "post":
+            if param_type == "form":
+                if header == "":
+                    r = requests.post(url, data=payload)
+                else:
+                    r = requests.post(url, data=payload, headers=header)
+
+            if param_type == "json":
+                if header == "":
+                    r = requests.post(url, json=payload)
+                else:
+                    r = requests.post(url, json=payload, headers=header)
+        return JsonResponse({"result": r.text})
+
+class CaseAssertView(View):
+    @method_decorator(login_required)
+    def post(self, request):
+        result_text = request.POST.get('result', '')
+        assert_text = request.POST.get('assert', '')
+        assert_method = request.POST.get('assert_method', '')
+        if result_text == "" or assert_text == "":
+            return JsonResponse({"result": "断言的文本不能为空"})
+        if assert_method == "contains":
+            if assert_text not in result_text:
+                return JsonResponse({"result": "断言失败"})
+            return JsonResponse({"result": "断言成功"})
+        if assert_method == "equals":
+            if assert_text == result_text:
+                return JsonResponse({"result": "断言成功"})
+            return JsonResponse({"result": "断言失败"})
+
+
+
 
